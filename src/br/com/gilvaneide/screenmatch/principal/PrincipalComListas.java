658862ab114d0557b1b01bd1cdd85ed3b3b7e2d6
Gilvaneide Medeiros <gilvaneide.m.bertaccini@gmail.com>
2025-06-30 23:15:02 -0300

    Commit inicial.

---
+++ src/br/com/gilvaneide/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,14 @@
+package br.com.gilvaneide.screenmatch.principal;
+
+import br.com.gilvaneide.screenmatch.modelos.Filme;
+import br.com.gilvaneide.screenmatch.modelos.Serie;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme filme1 = new Filme("O poderoso chefão", 1970);
+        Filme filme2 = new Filme("Avatar", 2023);
+        Filme filme3 = new Filme("A lista de Schindler", 1993);
+        Serie serie1 = new Serie("Lost", 2000);
+    }
+
+}
