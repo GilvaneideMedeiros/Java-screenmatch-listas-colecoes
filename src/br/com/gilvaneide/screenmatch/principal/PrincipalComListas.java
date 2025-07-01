0064742ea6905b51625fe1ad1dbed29b1f1548da
Gilvaneide Medeiros <gilvaneide.m.bertaccini@gmail.com>
2025-07-01 00:41:33 -0300

    Criação de listas com ArrayList. Uso do for-each para mostrar a lista de títulos (filmes e série).

--- src/br/com/gilvaneide/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/gilvaneide/screenmatch/principal/PrincipalComListas.java
@@ -2,15 +2,25 @@ package br.com.gilvaneide.screenmatch.principal;
 
 import br.com.gilvaneide.screenmatch.modelos.Filme;
 import br.com.gilvaneide.screenmatch.modelos.Serie;
+import br.com.gilvaneide.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
-        Filme filme1 = new Filme("O poderoso chefão", 1970);
-        Filme filme2 = new Filme("Avatar", 2023);
-        Filme filme3 = new Filme("A lista de Schindler", 1993);
-        Serie serie1 = new Serie("Lost", 2000);
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        Filme filmeDoPaulo = new Filme("A lista de Schindler", 1993);
+        Serie minhaSerie = new Serie("Lost", 2000);
 
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(filmeDoPaulo);
+        lista.add(minhaSerie);
+        for (Titulo item : lista) {
+            System.out.println(item);
+        }
 
     }
-
 }
